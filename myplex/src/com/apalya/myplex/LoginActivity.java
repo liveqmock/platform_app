471eeb5fa0f2ddfebf4402aab520cc79797a9d16
Amlan <amlana.dwivedy@apalya.myplex.tv>
2014-02-27 18:07:28 +0530

    Crashanalytics stoped revoked

--- myplex/src/com/apalya/myplex/LoginActivity.java
+++ myplex/src/com/apalya/myplex/LoginActivity.java
@@ -197,7 +197,7 @@ GooglePlayServicesClient.OnConnectionFailedListener, PlusClient.OnPersonLoadedLi
 		};
 		task.execute();
 
-//		Crashlytics.start(this);
+		Crashlytics.start(this);
 
 		FontUtil.loadFonts(getAssets());
 		String trackingDistinctId = getTrackingDistinctId();
